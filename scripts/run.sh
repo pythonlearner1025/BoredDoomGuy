3e0a9a0a8f3c5be665043d052b32ec4f8d8945b1
pythonlearner1025 <mjsong2021@gmail.com>
2025-10-20 21:50:42 -0700

    organize

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,4 +1,2 @@
-python agent_simple.py \
-      --model Arnold/dumped/doom1_singleplayer/9sv3tkmdrn/periodic-350000.pth \
-      --params Arnold/dumped/doom1_singleplayer/9sv3tkmdrn/params.pkl \
-      --duration 120
+python -W ignore::UserWarning idm_multi.py
+
